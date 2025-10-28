8d5f5314d7c410961de423042ebde5f8a77d1475
monicadeus <deusmonica06@gmail.com>
2025-10-28 17:55:19 -0300

    App con comentarios

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 from flask import Flask, request, render_template, flash, redirect
-import os
+'''
+Flask - crea la aplicación web
+request - permite acceder a los archivos que el usuario envía
+render_template - carga un archivo HTML desde la carpeta "templates"
+flash - muestra mensajes temporales
+redirect - redirije al usuario a otra ruta
+'''
 
-app = Flask(__name__)
+import os  #sirve para manejar rutas y archivos en el sistema operativo
+
+app = Flask(__name__)  #crea la app
 app.secret_key = "clave_secreta"  # Necesario para mostrar mensajes temporales
 
 # Carpeta donde se guardan los archivos
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = {"mp3", "wav", "flac", "ogg"}
 
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER #guarda la configuración para oder usarla luego
 
 # Crear la carpeta si no existe
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+#Validación de los archivos
 def allowed_file(filename):
     """Verifica que la extensión sea válida."""
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=["GET", "POST"])
+#se ejecuta cada vez que alguien entre ala web (GET) o suba un archivo (POST)
 def upload_file():
     if request.method == "POST":
         # Comprobamos si el formulario tiene un archivo
@@ -26,11 +36,12 @@ def upload_file():
             return redirect(request.url)
 
         file = request.files["music_file"]
-
+        #también avisa si no se selecciona ningún archivo
         if file.filename == "":
             flash("Nombre de archivo vacío.")
             return redirect(request.url)
 
+        #guardar el archivo en uploads si es vá;ido
         if file and allowed_file(file.filename):
             filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
             file.save(filepath)
@@ -40,7 +51,9 @@ def upload_file():
             flash("Tipo de archivo no permitido. (Usa mp3, wav, flac u ogg)")
             return redirect(request.url)
 
+    #si el metodo es GET, carga el HTML
     return render_template("upload.html")
 
+#activa el modo debug, haciendo que se reinicie automáticamente cuando cambias el código
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8080)
+    app.run(debug=True)
